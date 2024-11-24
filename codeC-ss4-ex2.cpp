4ac14b914569ecb0acc0cd213184350ddc7d255f
hieutitz <nt.hieu616@gmail.com>
2024-11-25 01:43:00 +0700

    Add files via upload

---
+++ codeC-ss4-ex2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int so_nguyen;
+    printf("Nhap vao mot so nguyen: ");
+    scanf("%d", &so_nguyen);
+    if (so_nguyen % 2 == 0) {
+        printf("So ban nhap la so chan\n");
+    } else {
+        printf("So ban nhap la so le\n");
+    }
+    return 0;
+}
+
